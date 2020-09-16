251ec250f2c7c96ed733486b73d6ee97043d1bda
Joel Denning <joeldenning@gmail.com>
2020-09-16 14:16:52 -0600

    Adding esmodule property to all externals

--- esm-webpack-plugin.js
+++ esm-webpack-plugin.js
@@ -99,9 +99,55 @@ function importsForModule(chunk, pluginOptions) {
             const request = typeof m.request === 'object' ? m.request.amd : m.request;
             const identifier = `__WEBPACK_EXTERNAL_MODULE_${Template.toIdentifier(`${m.id}`)}__`;
 
-            return `import * as ${identifier} from '${request}';`
+            return `import * as _${identifier} from '${request}'; var ${identifier} = cloneWithEsModuleProperty(_${identifier});`
         })
-        return [importStatements.join('\n'), "\n\n"];
+        return [importStatements.join('\n'), "\n\n"].concat(`
+        function cloneWithEsModuleProperty(ns) {
+            const result = Object.create(null);
+          
+            Object.defineProperty(result, "__esModule", {
+              value: true,
+              enumerable: false,
+              configurable: true,
+            });
+          
+            const propertyNames = Object.getOwnPropertyNames(ns);
+            for (let i = 0; i < propertyNames.length; i++) {
+              const propertyName = propertyNames[i];
+          
+              Object.defineProperty(result, propertyName, {
+                get: function () {
+                  return ns[propertyName];
+                },
+                enumerable: true,
+                configurable: false,
+              });
+            }
+          
+            if (Object.getOwnPropertySymbols) {
+              const symbols = Object.getOwnPropertySymbols(ns);
+          
+              for (let i = 0; i < symbols.length; i++) {
+                const symbol = symbols[i];
+          
+                Object.defineProperty(result, symbol, {
+                  get: function () {
+                    return ns[symbol];
+                  },
+                  enumerable: false,
+                  configurable: false,
+                });
+              }
+            }
+          
+            Object.preventExtensions(result);
+            Object.seal(result);
+            if (Object.freeze) {
+              Object.freeze(result);
+            }
+            return result;
+          }
+        `);
     } else {
         // Use default webpack behavior
         return [];
