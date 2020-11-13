87c76a3493273351523e30b665de6d9fa85474b0
Joel Denning <joeldenning@gmail.com>
2020-11-13 15:07:49 -0700

    Adding tests, refactor

--- esm-webpack-plugin.js
+++ esm-webpack-plugin.js
@@ -13,7 +13,10 @@ const defaultOptions = {
     skipModule: () => false,
 
     // Treat externals as globals, by default
-    moduleExternals: false
+    moduleExternals: false,
+
+    // Add __esModule property to all externals
+    esModuleExternals: true
 };
 
 
@@ -99,55 +102,81 @@ function importsForModule(chunk, pluginOptions) {
             const request = typeof m.request === 'object' ? m.request.amd : m.request;
             const identifier = `__WEBPACK_EXTERNAL_MODULE_${Template.toIdentifier(`${m.id}`)}__`;
 
-            return `import * as _${identifier} from '${request}'; var ${identifier} = cloneWithEsModuleProperty(_${identifier});`
+            return pluginOptions.esModuleExternals
+                ? `import * as $${identifier} from '${request}'; var ${identifier} = cloneWithEsModuleProperty($${identifier});`
+                : `import * as ${identifier} from '${request}';`
         })
-        return [importStatements.join('\n'), "\n\n"].concat(`
-        function cloneWithEsModuleProperty(ns) {
-            const result = Object.create(null);
-          
-            Object.defineProperty(result, "__esModule", {
-              value: true,
-              enumerable: false,
-              configurable: true,
-            });
-          
-            const propertyNames = Object.getOwnPropertyNames(ns);
-            for (let i = 0; i < propertyNames.length; i++) {
-              const propertyName = propertyNames[i];
-          
-              Object.defineProperty(result, propertyName, {
-                get: function () {
-                  return ns[propertyName];
-                },
-                enumerable: true,
-                configurable: false,
-              });
-            }
-          
-            if (Object.getOwnPropertySymbols) {
-              const symbols = Object.getOwnPropertySymbols(ns);
-          
-              for (let i = 0; i < symbols.length; i++) {
-                const symbol = symbols[i];
-          
-                Object.defineProperty(result, symbol, {
-                  get: function () {
-                    return ns[symbol];
-                  },
-                  enumerable: false,
-                  configurable: false,
-                });
-              }
-            }
-          
-            Object.preventExtensions(result);
-            Object.seal(result);
-            if (Object.freeze) {
-              Object.freeze(result);
-            }
-            return result;
-          }
-        `);
+
+        const result = [importStatements.join("\n")]
+
+        if (pluginOptions.esModuleExternals) {
+            // The code here was originally copied from https://github.com/joeldenning/add-esmodule
+            result.push(Template.asString([
+                "\n",
+                "function cloneWithEsModuleProperty(ns) {",
+                Template.indent([
+                    "const result = Object.create(null);",
+                    `Object.defineProperty(result, "__esModule", {`,
+                    Template.indent([
+                        `value: true,`,
+                        `enumerable: false,`,
+                        `configurable: true`,
+                    ]),
+                    "});",
+                    `const propertyNames = Object.getOwnPropertyNames(ns);`,
+                    `for (let i = 0; i < propertyNames.length; i++) {`,
+                    Template.indent([
+                        `const propertyName = propertyNames[i];`,
+                        `Object.defineProperty(result, propertyName, {`,
+                        Template.indent([
+                            `get: function () {`,
+                            Template.indent([
+                                `return ns[propertyName];`
+                            ]),
+                            `},`,
+                            `enumerable: true,`,
+                            `configurable: false,`
+                        ]),
+                        `});`
+                    ]),
+                    `}`,
+                    `if (Object.getOwnPropertySymbols) {`,
+                    Template.indent([
+                        `const symbols = Object.getOwnPropertySymbols(ns);`,
+                        `for (let i = 0; i < symbols.length; i++) {`,
+                        Template.indent([
+                            `const symbol = symbols[i];`,
+                            `Object.defineProperty(result, symbol, {`,
+                            Template.indent([
+                                `get: function () {`,
+                                Template.indent([
+                                    `return ns[symbol];`
+                                ]),
+                                `},`,
+                                `enumerable: false,`,
+                                `configurable: false,`,
+                            ]),
+                            `});`,
+                        ]),
+                        "}",
+                    ]),
+                    `}`,
+                    `Object.preventExtensions(result);`,
+                    `Object.seal(result);`,
+                    `if (Object.freeze) {`,
+                    Template.indent([
+                        `Object.freeze(result);`,
+                    ]),
+                    `}`,
+                    `return result;`,
+                ]),
+                `}`
+            ]))
+        }
+
+        result.push("\n");
+
+        return result
     } else {
         // Use default webpack behavior
         return [];
